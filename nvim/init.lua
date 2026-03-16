baa01af9d9d34bf2c89947a6c08726338988635d
Kabir Vaidya <kabirvinayakvaidya@gmail.com>
2026-03-16 13:29:17 +0000

    nvim config init

---
+++ nvim/init.lua
@@ -0,0 +1,26 @@
+require 'core.options'
+require 'core.keymaps'
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = false
+-- Setup lazy.nvim (Plugin Manager)
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then error('Error cloning lazy.nvim:\n' .. out) end
+end
+
+---@type vim.Option
+local rtp = vim.opt.rtp
+rtp:prepend(lazypath)
+
+-- NOTE: Here is where you install your plugins.
+require('lazy').setup({
+   	require 'plugins.colorscheme',
+    require 'plugins.snacks',
+    require 'plugins.arrow',
+    require 'plugins.statusline',
+    require 'plugins.tmux'
+})
+
